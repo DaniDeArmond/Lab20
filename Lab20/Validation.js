180f496a8191e1c4ccb78778063d4c10dd2a4ce1
dani.dearmond <dani.dearmond@gmail.com>
2018-02-20 16:23:22 -0500

    validation is now working

---
+++ Lab20/Validation.js
@@ -0,0 +1,17 @@
+﻿
+function validate() {
+
+   
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var emailReg = "^[\@]+[.]+$";
+    if (name === '' || email === '') {
+        alert("Please fill all fields...!!!!!!");
+        return false;
+    } else if (!(email).match(emailReg)) {
+        alert("Invalid Email...!!!!!!");
+        return false;
+    } else {
+        return true;
+    }
+}
